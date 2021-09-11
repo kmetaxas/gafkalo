436e2681f0e85926342df28802b8d49e41dbd5a0
Konstantinos Metaxas <kmetaxas@gmail.com>
2021-09-11 23:01:15 +0200

    parse SCHEMA keys+values and add them to internal state

--- schemas_vroom.go
+++ schemas_vroom.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/Shopify/sarama"
 	"log"
@@ -19,29 +20,55 @@ This *can* require a lot of RAM in the case of huge amount of subjects and big s
 */
 
 type SchemaRegistryCache struct {
-	schemas      map[string]Schema
+	// Schema IDs
+	schemas      map[int]string
+	subjects     map[string]map[int]int
 	globalCompat string
 	consumer     *Consumer
-	ready        chan bool
-	ctx          context.Context // tell the consumer to stop
-	cancel       context.CancelFunc
+	//ready        chan bool
+	ctx    context.Context // tell the consumer to stop
+	cancel context.CancelFunc
+}
+
+// keytype SCHEMA in _schemas topic Key
+type SRKey struct {
+	Keytype string `json:"keytype"` // SCHEMA , CONFIG, NOOP
+	Subject string `json:"subject"`
+	Version int    `json:"version"`
+	Magic   int    `json:"magic"`
+}
+
+// Value for CONFIG type
+type SRValueConfig struct {
+	CompatibilityLevel string `json:"compatibilityLevel"`
+}
+
+// Value for SCHEMA type
+type SRValueSchema struct {
+	Subject string `json:"subject"`
+	Version int    `json:"version"`
+	Id      int    `json:"id"`
+	Schema  string `json:"schema"`
+	Deleted bool   `json:"deleted"`
 }
 
 func NewSchemaRegistryCache(config *Configuration) (*SchemaRegistryCache, error) {
 	var err error
 	topics := []string{"_schemas"}
 	var srCache SchemaRegistryCache
-	consumer := NewConsumer(config.Connections.Kafka, &config.Connections.Schemaregistry, topics, fmt.Sprintf("gafkalo-sr-%s", RandomString(4)), nil, false, false, false, true, "", srCache)
+	consumer := NewConsumer(config.Connections.Kafka, &config.Connections.Schemaregistry, topics, fmt.Sprintf("gafkalo-sr-%s", RandomString(4)), nil, false, false, false, true, "", &srCache)
 	srCache.consumer = consumer
+	srCache.schemas = make(map[int]string)
+	srCache.subjects = make(map[string]map[int]int)
 	return &srCache, err
 }
 
 func (c *SchemaRegistryCache) readSchemaTopic(topic string) {
+	// TODO read until the end and stop.
 	err := c.consumer.Consume(10)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 // Update the schema cache ith the provided schema object
@@ -59,21 +86,65 @@ func (c *SchemaRegistryCache) GetSchemaForSubject(name string) (*Schema, error) 
 }
 
 // Implement sarama consumer ConsumerGroupHandler interface
-func (r SchemaRegistryCache) Setup(session sarama.ConsumerGroupSession) error {
-	close(r.ready)
+func (r *SchemaRegistryCache) Setup(session sarama.ConsumerGroupSession) error {
+	close(r.consumer.ready)
 	return nil
 }
 
-func (r SchemaRegistryCache) Cleanup(session sarama.ConsumerGroupSession) error {
+func (r *SchemaRegistryCache) Cleanup(session sarama.ConsumerGroupSession) error {
 	return nil
 }
 
-func (r SchemaRegistryCache) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	log.Printf("CONSUME_CLAIN!!\n")
+func (r *SchemaRegistryCache) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
-		log.Printf("SR_KEY=%+vs\n", message.Key)
-		log.Printf("SR_Value=%+vs\n", message.Value)
+		log.Printf("SR_KEY=%+v\n", string(message.Key))
+		var recordKey SRKey
+		err := json.Unmarshal(message.Key, &recordKey)
+		if err != nil {
+			return err
+		}
+		switch recordKey.Keytype {
+		case "CONFIG":
+			r.processConfigValue(&recordKey, message.Value)
+		case "SCHEMA":
+			r.processSchemaValue(&recordKey, message.Value)
+		}
+		//log.Printf("SR_Value=%+vs\n", string(message.Value))
 		session.MarkMessage(message, "")
 	}
+	return nil
+}
+
+func (r *SchemaRegistryCache) processConfigValue(key *SRKey, data []byte) error {
+	var value SRValueConfig
+	err := json.Unmarshal(data, &value)
+	if err != nil {
+		return err
+	}
+	log.Printf("VALUE_CONFIG=%+v\n", value)
+	return nil
+}
+
+// Add a subject to known subjects. Handled nested maps properly
+func (r *SchemaRegistryCache) addSubject(name string, version, id int) {
+	_, exists := r.subjects[name]
+	if !exists {
+		r.subjects[name] = make(map[int]int)
+	}
+	r.subjects[name][version] = id
+}
+
+// Process the Kafka record with a keytype=SCHEMA. ACcepts the binary value and needs to be unmarshalled and processed
+func (r *SchemaRegistryCache) processSchemaValue(key *SRKey, data []byte) error {
+	var value SRValueSchema
+	err := json.Unmarshal(data, &value)
+	if err != nil {
+		return err
+	}
+	// We don't check if it exists already as any event replaces the previous one.
+	// Since this is a compacted topic, if a key exists then we create a map entry.
+	// TODO handle tombstones!
+	r.schemas[value.Id] = value.Schema
+	r.addSubject(value.Subject, value.Version, value.Id)
 	return nil
 }
